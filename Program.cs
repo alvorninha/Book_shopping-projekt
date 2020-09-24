592c33db767f2b15396e61eb10fec0c093ba2497
alvorninha <66682526+alvorninha@users.noreply.github.com>
2020-09-24 09:14:29 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace BookShopping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string exit;
+            string user_input;
+            while (true)
+            {
+                user_input = Console.ReadLine();
+                showspecific(user_input);
+
+                exit = System.Console.ReadLine();
+
+
+                if (exit == "EXIT")
+                {
+                    break;
+                }
+            }
+        }
+
+
+        static void showspecific(string search)
+        {
+            int counter = 0;
+            string line;
+            System.IO.StreamReader file =
+            new System.IO.StreamReader(@"I:\spiegel-bestseller.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                if (line.Contains(search))
+                {
+                    System.Console.WriteLine(line);
+                }
+                counter++;
+            }
+
+            file.Close();
+
+
+        }
+
+
+
+    }
+}
